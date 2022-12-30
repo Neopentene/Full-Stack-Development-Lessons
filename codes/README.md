232e80e4361cb8f7145f3a527098a85a3d4a5ffb
Neopentene <celestynkinny3@gmail.com>
2022-12-31 00:52:50 +0530

    added basic css, fixed bugs, and changed code render method

---
+++ codes/README.md
@@ -0,0 +1,16 @@
+# Code snippets used in lessons
+
+## Xml Rules
+
+- The root element is always the following `<codes></codes>`
+- Javascript codes are surrounded by the following tag `<javascript></javascript>`
+- Css styles are surrounded by the following tag `<css></css>`
+- HTML codes are surrounded by the following tag `<rawhtml></rawhtml>`
+- For any other type of code format the following tag is used `<render></render>`
+- All the above tags have the attribute `for`. This attribute is used to find the id of the code element that will host the code written inside. Usage Example: `<css for="id-name"></css>`
+- Remember that `<![CDATA[]>` is used for markups
+
+### Note
+
+- Names of the xml files are the same as the lessons they are used in
+- Also a custom data attribute is added on every body tag to locate the file in which the code snippets are kept. Usage Example: `<body data-file="xml-file-name"></body>`. The name must not contain the extension.
---
+++ scripts/code.js
@@ -0,0 +1,81 @@
+function filterHtml(html = "") {
+  let codes = {
+    "<": "&lt;",
+    ">": "&gt;",
+  };
+
+  let filteredHtml = "";
+
+  for (let i = 0; i < html.length; i++) {
+    let replacement = codes[html[i]];
+    filteredHtml += replacement ? replacement : html[i];
+  }
+
+  return filteredHtml;
+}
+
+function generateNotification(text = "", id = "") {
+  let div = document.createElement("div");
+  div.id = id + "-notification";
+  div.textContent = text;
+  div.classList.add("notification");
+
+  return div;
+}
+
+let fileName = document.body.getAttribute("data-file");
+
+async function onload() {
+  await fetch(`/codes/${fileName}.xml`)
+    .then((response) => response.text())
+    .then((rawXML) => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(rawXML, "text/xml");
+
+      const root = xml.documentElement;
+
+      root.childNodes.forEach((node) => {
+        let id = node.nodeName == "#text" ? null : node.getAttribute("for");
+        console.log(id);
+
+        if (id) {
+          let el = document.getElementById(id);
+
+          if (el) {
+            el.innerHTML = filterHtml(node.textContent.trim());
+            hljs.highlightElement(el);
+          }
+        }
+      });
+    });
+
+  document.querySelectorAll(".copy").forEach((el) => {
+    el.addEventListener("click", () => {
+      let id = el.getAttribute("data-for");
+      let range = document.createRange();
+      let code = document.getElementById(el.getAttribute("data-for"));
+
+      range.selectNodeContents(code);
+
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+
+      let notification = generateNotification(
+        el.getAttribute("data-message"),
+        id
+      );
+
+      document.body.appendChild(notification);
+
+      setTimeout(() => {
+        document.body.removeChild(notification);
+      }, 1000);
+
+      document.execCommand("copy");
+
+      window.getSelection().removeAllRanges();
+    });
+  });
+}
+
+onload();
