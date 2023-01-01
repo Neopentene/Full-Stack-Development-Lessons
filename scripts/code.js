4084f8520cf02c01e48265b7037e413a70ae6e5e
Neopentene <celestynkinny3@gmail.com>
2023-01-02 00:05:24 +0530

    Auto Generate HTML boilerplate

--- scripts/code.js
+++ scripts/code.js
@@ -7,8 +7,7 @@ function generateCodeBlock(id = "", message = "") {
   const div = document.createElement("div");
 
   code.id = id;
-  div.classList.add("copy");
-  div.classList.add("disabled");
+  div.classList.add("copy disabled");
   div.setAttribute("data-for", id);
   if (message && message != "") div.setAttribute("data-message", message);
 
